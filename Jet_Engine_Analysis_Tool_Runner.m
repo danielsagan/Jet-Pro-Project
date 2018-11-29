9debb77e3096c1ee059bcdca4cf343dfba1bb600
Daniel Sagan <daniel@lawn-128-61-113-31.lawn.gatech.edu>
2018-11-28 20:39:19 -0500

    Made it more user friendly
    Seems to work

--- Jet_Engine_Analysis_Tool_Runner.m
+++ Jet_Engine_Analysis_Tool_Runner.m
@@ -5,13 +5,10 @@ global constraints;
 x0 = [40.7377,1.1001,0.3043,0.0093,0,1.9672*10^(-6)]';
 lb = [1 1.1 0 0 0 0];
 ub = [55 1.7 8 1 0 0.1];
-%options = optimoptions('fmincon','Algorithm','active-set','Display','iter');
-
-%[x,fval,exitflag,output] = fmincon(@TSFCFun,x0,[],[],[],[],lb,ub,@mycon)
 
 bestTSFC = 1000;
 
-for i = 1:1
+for i = 1:10
     
     x0 = [rand*(ub(1) - lb(1)) + lb(1), rand*(ub(2) - lb(2)) + lb(2), rand*(ub(3) - lb(3)) + lb(3), rand*(ub(4) - lb(4)) + lb(4), rand*(ub(5) - lb(5)) + lb(5), rand*(ub(6) - lb(6)) + lb(6)];
     
